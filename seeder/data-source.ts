11fd935c5ec9686d065b9b8c76306d10aa772dcd
Brezzor <oliver.bresson@gmail.com>
2025-10-01 20:35:01 +0200

    Minor changes to seeder

--- seeder/data-source.ts
+++ seeder/data-source.ts
@@ -3,13 +3,14 @@ import { Games } from "./entities/Games.js";
 import { Genres } from "./entities/Genres.js";
 import { ParentPlatforms } from "./entities/ParentPlatforms.js";
 import { Stores } from "./entities/Stores.js";
+import "dotenv/config"
 
 const AppDataSource = new DataSource({
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "admin",
-    database: "rawg_db",
+    host: process.env.DB_HOST ?? "localhost",
+    port: (process.env.DB_PORT as unknown as number) ?? 3306,
+    username: process.env.DB_USERNAME ?? "root",
+    password: process.env.DB_PASSWORD ?? "admin",
+    database: process.env.DB_DATABASE ?? "rawg_db",
     type: "mysql",
     synchronize: true,
     logging: true,
