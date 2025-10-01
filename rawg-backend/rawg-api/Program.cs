ac23434060ca3c3db66857e1fba693898d73ef6a
Brezzor <oliver.bresson@gmail.com>
2025-10-01 23:15:01 +0200

    Remove appsettings momentarily and added ef core models.

--- rawg-backend/rawg-api/Program.cs
+++ rawg-backend/rawg-api/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using rawg_api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<rawg_dbContext>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("Default"),
+        new MySqlServerVersion(new Version(9, 4, 0)));
+});
 
 var app = builder.Build();
 
